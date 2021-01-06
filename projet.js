b23153b6bf30c52f7ae14793b681def6f8dddb3e
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-06 18:00:00 +0100

    function forward

--- projet.js
+++ projet.js
@@ -1,7 +1,7 @@
 // MARS ROVER 
 
 
-// 01 
+// 01 - Global Variables
 
 var grid = [
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -23,24 +23,24 @@ var rover = {
 };
 
 
-// 02 
+// 02 - Functions Left Right
+
 function turnLeft(rover) {
     if (rover.direction === "N") {
         rover.direction = "W";
-        console.log("direction NW", rover.direction);
+        console.log("Direction NW", rover.direction);
     } else if (rover.direction === "W") {
         rover.direction = "S";
-        console.log(rover)
+        console.log("Direction WS", rover.direction);
     } else if (rover.direction === "S") {
         rover.direction = "E";
-        console.log(rover)
+        console.log("Direction SE", rover.direction);
     } else {
-        rover.direction = "S";
-        console.log(rover);
+        rover.direction = "N";
+        console.log("Direction EN", rover.direction);
     }
     console.log(rover);
-}
-
+};
 
 
 function turnRight(rover) {
@@ -50,29 +50,38 @@ function turnRight(rover) {
         console.log("Direction NE", rover.direction);
     } else if (rover.direction === "E") {
         rover.direction = "S";
+        console.log("Direction ES", rover.direction);
     } else if (rover.direction === "S") {
         rover.direction = "W";
+        console.log("Direction SW", rover.direction);
     } else {
-        rover.direction = "N"
+        rover.direction = "N";
+        console.log("Direction WN", rover.direction);
     };
-
     console.log(rover);
-}
-turnRight(rover);
+};
 
-// 03 -  Le déplacement
+turnRight(rover);
+turnLeft(rover);
+
+
+// 04 -  Function Forward
 
 function moveForward(rover) {
     if (rover.direction === "N") {
         rover.y = rover.y - 1;
+        console.log("Forward N", rover.y);
     } else if (rover.direction === "S") {
         rover.y = rover.y + 1;
+        console.log("Forward S", rover.y);
     } else if (rover.direction === "E") {
         rover.x = rover.x + 1;
+        console.log("Forward E", rover.x);
     } else {
         rover.x = rover.x - 1;
+        console.log("Forward W", rover.x);
     }
-
     console.log(rover)
 }
+
 moveForward(rover);
